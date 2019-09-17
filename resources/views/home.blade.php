68dfe6f75238cbfb0b6e5549adcd68366c6d0814
Olamide <olakehinde59@gmail.com>
2019-09-17 14:46:21 +0100

    modified the login welcome message to include user name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome, {{Auth::user()->name}}. You are logged in!
                 </div>
             </div>
         </div>
