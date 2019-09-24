acfc40c79d2eca977b0cbbec14a2560e5770a752
Olamide <olakehinde59@gmail.com>
2019-09-24 14:03:33 +0100

    added index function to webstore controller

--- app/Http/Controllers/WebstoreController.php
+++ app/Http/Controllers/WebstoreController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Gloudemans\Shoppingcart\Cart;
 
 class WebstoreController extends Controller
 {
-    //
+    public function index() {
+    	$products = Product::all();
+
+    	return view('store')->with('products', $products);
+    }
 }
